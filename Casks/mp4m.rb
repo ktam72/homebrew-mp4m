9ae9bb478efe7bcd3255765c4a52934e78b3c40e
ktam <ktam72@gmail.com>
2026-08-25 11:43:27 +0900

    Update MP4M cask to 3.3.2

--- Casks/mp4m.rb
+++ Casks/mp4m.rb
@@ -1,6 +1,6 @@
 cask "mp4m" do
-  version "3.3.1"
-  sha256 "5d5078de9a63ff6505f6a5061603eb2def8b73e1d4b69d9d06be98096a9c16ef"
+  version "3.3.2"
+  sha256 "ef0f90558767f920197c73eb29c5f1de3ace5afdfd2a3ee48b47d9ab67b3a8f5"
 
   url "https://github.com/ktam72/mp4m-public/releases/download/#{version}/MP4M-#{version}.zip",
       verified: "github.com/ktam72/mp4m-public/"
